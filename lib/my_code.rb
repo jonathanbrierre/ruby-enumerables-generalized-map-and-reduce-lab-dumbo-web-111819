2f117a935989f2702e104ebc6d9d06e38e0b00ef
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-21 02:45:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,12 +4,14 @@ def map_stuff(source_array)
   new_array =[]
   i = 0 
   while i < source_array.count do
-    yield 
-    new_array << source_array[i]
+    
+    new_array << yield(source_array[i])
+    i += 1 
     
   end
+  new_array
 end
 
-map_stuff ([1,2,3,-9]) {|n| n * -1}
+
 # map {|n| n}
 # map {|n| n*n}
